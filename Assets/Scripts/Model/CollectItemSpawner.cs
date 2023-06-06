b58ab62382581a6c55fae65a8bef042ff420c14e
alex.a.om.temp <alex.a.om.temp@gmail.com>
2023-06-06 20:42:36 +0300

    gui changed

--- Assets/Scripts/Model/CollectItemSpawner.cs
+++ Assets/Scripts/Model/CollectItemSpawner.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class CollectItemSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject _collectItemPrefab;
+    [SerializeField] private GameObject _collectItemsFolder;
     [SerializeField] private RectTransform _demonTransform;
     [SerializeField] private RectTransform _groundTransform;
     [SerializeField] private CombatSystem _enemyCombatSystem;
@@ -16,7 +17,7 @@ public class CollectItemSpawner : MonoBehaviour
     void CreateItem(ItemNames name)
     {
         var prefab = Instantiate (_collectItemPrefab, _demonTransform.position, 
-            Quaternion.identity, _demonTransform.transform);
+            Quaternion.identity, _collectItemsFolder.transform);
         prefab.GetComponent<RectTransform>().localScale = new Vector3(-1,1,1);
         prefab.GetComponent<CollectFall>().Init(_groundTransform);
         prefab.GetComponent<CollectItemButton>().Init(name,_playerCombatSystem);
